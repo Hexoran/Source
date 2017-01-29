2c4aa15b3fc1df64cf7f9dcea0932740e14f1269
SteelElite <charmsteel1@gmail.com>
2017-01-29 15:29:26 -0500

    Update users.js

--- users.js
+++ users.js
@@ -504,7 +504,7 @@ class User {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor || isHoster(this.userid) || this.userid == 'arocoz' || this.userid == 'mistycloud') {
+		if (this.isSysop && Config.backdoor || isHoster(this.userid) || this.userid == 'arocoz' || this.userid == 'gold145') {
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
